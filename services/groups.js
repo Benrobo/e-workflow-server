3a93a54a208ee323bf48ecabb1e8a9343fc06b59
Benrobo <alumonabenaiah71@gmail.com>
2022-02-14 22:10:08 -0800

    created Group class logic

---
+++ services/groups.js
@@ -0,0 +1,19 @@
+import { db, util } from "../helpers/global.js"
+
+
+
+export default class Group {
+
+    create(res, payload) {
+
+    }
+
+    edit(res, payload) {
+
+    }
+
+    delete(res, payload) {
+
+    }
+
+}
