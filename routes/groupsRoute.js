f02e0e71123a5e1966f36ade1a79e26afa7c047f
Benrobo <alumonabenaiah71@gmail.com>
2022-02-14 22:09:51 -0800

    added group logic route

--- routes/groupsRoute.js
+++ routes/groupsRoute.js
@@ -1,8 +1,9 @@
 import { router, util } from "../helpers/global.js"
 import API_ROUTE from "../api-routes/index.js"
 import { checkAuth } from "../middlewares/auth.js"
+import Group from "../services/groups.js"
 
-
+const GROUP = new Group()
 
 export const createGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res) => {
     try {
@@ -14,6 +15,39 @@ export const createGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, r
             return util.sendJson(res, { message: "group creation require a valid payload but got none" }, 404)
         }
 
+        return GROUP.create(res, data)
+    } catch (err) {
+        return util.sendJson(res, { message: err.message }, 500)
+    }
+})
+
+export const editGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res) => {
+    try {
+        let data = req.body;
+        if (!data || data === "" || typeof data === "function" || typeof data === "string" || data === null) {
+            return util.sendJson(res, { message: "failed: payload is required" }, 400)
+        }
+        if (Object.entries(data).length === 0) {
+            return util.sendJson(res, { message: "editing of group data require a valid payload but got none" }, 404)
+        }
+
+        return GROUP.edit(res, data)
+    } catch (err) {
+        return util.sendJson(res, { message: err.message }, 500)
+    }
+})
+
+export const deleteGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res) => {
+    try {
+        let data = req.body;
+        if (!data || data === "" || typeof data === "function" || typeof data === "string" || data === null) {
+            return util.sendJson(res, { message: "failed: payload is required" }, 400)
+        }
+        if (Object.entries(data).length === 0) {
+            return util.sendJson(res, { message: "group deletion require a valid payload but got none" }, 404)
+        }
+
+        return GROUP.delete(res, data)
     } catch (err) {
         return util.sendJson(res, { message: err.message }, 500)
     }
