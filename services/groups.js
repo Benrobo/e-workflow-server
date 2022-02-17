7bb92dc8e1c611a6cdf5cbc04d9cb3e1e223e58f
Benrobo <alumonabenaiah71@gmail.com>
2022-02-16 22:01:34 -0800

    added editing and deleting of both members and group

--- services/groups.js
+++ services/groups.js
@@ -1,15 +1,5 @@
 import { db, util } from "../helpers/global.js"
 
-/**
- * @DataPayload
- * {
-    "studentId": "ace32d77-4607-47ed-999a-f1ecb4c2f4f2",
-    "name": "Group Amazon",
-    "courseName": "Intro to Operating System",
-    "courseType": "Computer Science"
-   }
-*/
-
 export default class Group {
 
     create(res, payload) {
@@ -180,16 +170,70 @@ export default class Group {
         }
     }
 
-    edit(res, payload) {
+    editGroup(res, payload) {
         if (res === "" || res === undefined || res === null) {
             return "group creation requires a valid {res} object but got none"
         }
 
         if (payload && Object.entries(payload).length > 0) {
-
-
+            if (payload.userId === undefined || payload.groupId === undefined || payload.courseName === undefined || payload.courseType === undefined) {
+                return util.sendJson(res, { error: true, message: "payload requires a valid fields [userId,courseName,courseType, groupId] but got undefined" }, 400)
+            }
+            if (payload.userId === "") {
+                return util.sendJson(res, { error: true, message: "userId cant be empty" }, 400)
+            }
+            if (payload.groupId === "") {
+                return util.sendJson(res, { error: true, message: "groupId cant be empty" }, 400)
+            }
+            if (payload.courseName === "") {
+                return util.sendJson(res, { error: true, message: "courseName cant be empty" }, 400)
+            }
+            if (payload.courseType === "") {
+                return util.sendJson(res, { error: true, message: "courseType cant be empty" }, 400)
+            }
             // check if user exist
             try {
+                const sql = `SELECT * FROM users WHERE "userId"=$1`
+                db.query(sql, [payload.userId], (err, result) => {
+                    if (err) {
+                        return util.sendJson(res, { error: true, message: err.message }, 400)
+                    }
+
+                    if (result.rowCount === 0) {
+                        return util.sendJson(res, { error: false, message: "user with that id dont exists: " + payload.userId }, 404)
+                    }
+
+                    // check if user already exist in a group before creating editing group info
+                    const sql2 = `SELECT * FROM groups WHERE id=$1`
+                    db.query(sql2, [payload.groupId], (err, data1) => {
+                        if (err) {
+                            return util.sendJson(res, { error: true, message: err.message }, 400)
+                        }
+
+                        if (data1.rows.length === 0) {
+                            return util.sendJson(res, { error: false, message: `fail to edit group, no group found` }, 404)
+                        }
+
+                        let membersIds = data1.rows[0].usersId;
+
+                        // check if userid trying to edit group detail is present
+                        if (membersIds.includes(payload.userId) === false) {
+                            return util.sendJson(res, { error: false, message: `cant edit group you dont belong to` }, 404)
+                        }
+
+                        const { name, courseName, courseType, groupId } = payload;
+
+                        const sql3 = `UPDATE groups SET "courseName"=$1, "courseType"=$2, "name"=$3 WHERE id=$4`
+                        db.query(sql3, [courseName.trim(), courseType.trim(), name.trim(), groupId.trim()], (err) => {
+                            if (err) {
+                                return util.sendJson(res, { error: true, message: err.message }, 400)
+                            }
+
+                            return util.sendJson(res, { error: false, message: "successfully edited group info" }, 200)
+                        })
+
+                    })
+                })
 
             } catch (err) {
                 console.log(err);
@@ -198,8 +242,143 @@ export default class Group {
         }
     }
 
-    delete(res, payload) {
+    deleteMemebers(res, payload) {
+        if (res === "" || res === undefined || res === null) {
+            return "group creation requires a valid {res} object but got none"
+        }
 
+        if (payload && Object.entries(payload).length > 0) {
+            if (payload.userId === undefined || payload.groupId === undefined || payload.memberId === undefined) {
+                return util.sendJson(res, { error: true, message: "payload requires a valid fields [userId,memberId, groupId] but got undefined" }, 400)
+            }
+            if (payload.userId === "") {
+                return util.sendJson(res, { error: true, message: "userId cant be empty" }, 400)
+            }
+            if (payload.groupId === "") {
+                return util.sendJson(res, { error: true, message: "groupId cant be empty" }, 400)
+            }
+            if (payload.memberId === "") {
+                return util.sendJson(res, { error: true, message: "memberId cant be empty" }, 400)
+            }
+            // check if user exist
+            try {
+                const sql = `SELECT * FROM users WHERE "userId"=$1 AND "userId"=$2`
+                db.query(sql, [payload.userId, payload.memberId], (err, result) => {
+                    if (err) {
+                        return util.sendJson(res, { error: true, message: err.message }, 400)
+                    }
+
+                    if (result.rowCount === 0) {
+                        return util.sendJson(res, { error: false, message: "user with that id dont exists: " + payload.userId }, 404)
+                    }
+
+                    // check if user already exist in a group before creating editing group info
+                    const sql2 = `SELECT * FROM groups WHERE id=$1`
+                    db.query(sql2, [payload.groupId], (err, data1) => {
+                        if (err) {
+                            return util.sendJson(res, { error: true, message: err.message }, 400)
+                        }
+
+                        if (data1.rows.length === 0) {
+                            return util.sendJson(res, { error: false, message: `fail to edit group, no group found` }, 404)
+                        }
+
+                        let membersIds = data1.rows[0].usersId;
+
+                        // check if userid trying to delete group member detail is present
+                        if (membersIds.includes(payload.userId) === false || membersIds.includes(payload.memberId) === false) {
+                            return util.sendJson(res, { error: false, message: `cant delete group members: cause member doesnt exists` }, 404)
+                        }
+
+                        let restMembers = membersIds.filter((list) => {
+                            return list !== payload.memberId
+                        })
+
+                        const { groupId } = payload;
+
+                        const sql3 = `UPDATE groups SET "usersId"=$1 WHERE id=$2`
+                        db.query(sql3, [restMembers, groupId.trim()], (err) => {
+                            if (err) {
+                                return util.sendJson(res, { error: true, message: err.message }, 400)
+                            }
+
+                            return util.sendJson(res, { error: false, message: "successfully deleted group member" }, 200)
+                        })
+
+                    })
+                })
+
+            } catch (err) {
+                console.log(err);
+                return util.sendJson(res, { error: false, message: err.message }, 500)
+            }
+        }
+    }
+
+    deleteGroup(res, payload) {
+        if (res === "" || res === undefined || res === null) {
+            return "group creation requires a valid {res} object but got none"
+        }
+
+        if (payload && Object.entries(payload).length > 0) {
+            if (payload.userId === undefined || payload.groupId === undefined) {
+                return util.sendJson(res, { error: true, message: "payload requires a valid fields [userId, groupId] but got undefined" }, 400)
+            }
+            if (payload.userId === "") {
+                return util.sendJson(res, { error: true, message: "userId cant be empty" }, 400)
+            }
+            if (payload.groupId === "") {
+                return util.sendJson(res, { error: true, message: "groupId cant be empty" }, 400)
+            }
+            // check if user exist
+            try {
+                const sql = `SELECT * FROM users WHERE "userId"=$1`
+                db.query(sql, [payload.userId], (err, result) => {
+                    if (err) {
+                        return util.sendJson(res, { error: true, message: err.message }, 400)
+                    }
+
+                    if (result.rowCount === 0) {
+                        return util.sendJson(res, { error: false, message: "user with that id dont exists: " + payload.userId }, 404)
+                    }
+
+                    // check if user already exist in a group before creating editing group info
+                    const sql2 = `SELECT * FROM groups WHERE id=$1`
+                    db.query(sql2, [payload.groupId], (err, data1) => {
+                        if (err) {
+                            return util.sendJson(res, { error: true, message: err.message }, 400)
+                        }
+
+                        if (data1.rows.length === 0) {
+                            return util.sendJson(res, { error: false, message: `fail to delete group, you dont belong to the group id provided` }, 404)
+                        }
+
+                        let membersIds = data1.rows[0].usersId;
+
+                        // check if userid trying to delete group member detail is present
+                        if (membersIds.includes(payload.userId) === false) {
+                            return util.sendJson(res, { error: false, message: `cant delete group members: cause member doesnt exists` }, 404)
+                        }
+
+                        const { groupId } = payload;
+
+                        const sql3 = `DELETE FROM groups WHERE id=$1`
+                        db.query(sql3, [groupId.trim()], (err) => {
+                            if (err) {
+                                return util.sendJson(res, { error: true, message: err.message }, 400)
+                            }
+
+                            return util.sendJson(res, { error: false, message: "successfully deleted group" }, 200)
+                        })
+
+                    })
+                })
+
+            } catch (err) {
+                console.log(err);
+                return util.sendJson(res, { error: false, message: err.message }, 500)
+            }
+        }
     }
 
 }
