32d872589ad3b465f18d7732f91f6a644837d605
Benrobo <alumonabenaiah71@gmail.com>
2022-02-16 22:01:45 -0800

    updated group route

--- routes/groupsRoute.js
+++ routes/groupsRoute.js
@@ -37,7 +37,7 @@ export const addMembers = router.post(API_ROUTE.addGroupMembers, checkAuth, (req
     }
 })
 
-export const editGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res) => {
+export const editGroup = router.post(API_ROUTE.editGroup, checkAuth, (req, res) => {
     try {
         let data = req.body;
         if (!data || data === "" || typeof data === "function" || typeof data === "string" || data === null) {
@@ -47,13 +47,14 @@ export const editGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res
             return util.sendJson(res, { message: "editing of group data require a valid payload but got none" }, 404)
         }
 
-        return GROUP.edit(res, data)
+        return GROUP.editGroup(res, data)
     } catch (err) {
         return util.sendJson(res, { message: err.message }, 500)
     }
 })
 
-export const deleteGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, res) => {
+
+export const deleteGroupMembers = router.post(API_ROUTE.deleteGroupMembers, checkAuth, (req, res) => {
     try {
         let data = req.body;
         if (!data || data === "" || typeof data === "function" || typeof data === "string" || data === null) {
@@ -63,7 +64,23 @@ export const deleteGroup = router.post(API_ROUTE.createGroup, checkAuth, (req, r
             return util.sendJson(res, { message: "group deletion require a valid payload but got none" }, 404)
         }
 
-        return GROUP.delete(res, data)
+        return GROUP.deleteMemebers(res, data)
+    } catch (err) {
+        return util.sendJson(res, { message: err.message }, 500)
+    }
+})
+
+export const deleteGroup = router.post(API_ROUTE.deleteGroup, checkAuth, (req, res) => {
+    try {
+        let data = req.body;
+        if (!data || data === "" || typeof data === "function" || typeof data === "string" || data === null) {
+            return util.sendJson(res, { message: "failed: payload is required" }, 400)
+        }
+        if (Object.entries(data).length === 0) {
+            return util.sendJson(res, { message: "group deletion require a valid payload but got none" }, 404)
+        }
+
+        return GROUP.deleteGroup(res, data)
     } catch (err) {
         return util.sendJson(res, { message: err.message }, 500)
     }
